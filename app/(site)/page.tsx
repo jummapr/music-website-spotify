06da7425c503ec0768a3bc0587b0560346a26419
jumma <aarifhingorja4@gmail.com>
2023-05-31 06:47:28 +0530

    starting to create ui

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,19 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
-   <div className="text-green-500">
-    Main Content
-   </div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2 ">
+          <h1 className="text-white text-3xl font-semibold ">
+            welcome Back
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem image="/images/liked.png" name="Liked songs" href="liked" />
+          </div>
+        </div>
+      </Header>
+    </div>
   );
 }
