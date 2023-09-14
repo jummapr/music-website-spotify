8b00d0bd14a146d1beeb92c40f9b595ad9bc2a39
jumma <aarifhingorja4@gmail.com>
2023-09-14 07:20:04 +0530

    added account page

--- app/api/create-portal-link/route.ts
+++ app/api/create-portal-link/route.ts
@@ -1,43 +1,36 @@
-import {createRouteHandlerClient} from "@supabase/auth-helpers-nextjs";
-import {cookies} from 'next/headers';
-import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from "next/headers";
+import { NextResponse } from 'next/server';
 
-
-import {stripe} from '@/libs/stripe';
-import { getURL } from "next/dist/shared/lib/utils";
-import { createOrRetrieveCustomer } from "@/libs/supabaseAdmin";
-
+import { stripe } from '@/libs/stripe';
+import { getURL } from '@/libs/helpers';
+import { createOrRetrieveCustomer } from '@/libs/supabaseAdmin';
 
 export async function POST() {
-    try {
-        const supabase = createRouteHandlerClient({
-            cookies
-        });
+  try {
+    const supabase = createRouteHandlerClient({ 
+      cookies
+     });
+    
+    const {
+      data: { user }
+    } = await supabase.auth.getUser();
 
+    if (!user) throw Error('Could not get user');
+    const customer = await createOrRetrieveCustomer({
+      uuid: user.id || '',
+      email: user.email || ''
+    });
 
-        const {data: {user}} = await supabase.auth.getUser();
+    if (!customer) throw Error('Could not get customer');
+    const { url } = await stripe.billingPortal.sessions.create({
+      customer,
+      return_url: `${getURL()}/account`
+    });
 
-        if(!user) {
-            throw new Error('could not get user');
-        };
-
-        const customer = await createOrRetrieveCustomer({
-            uuid: user.id || '',
-            email: user.email || ''
-        });
-
-        if(!customer) {
-            throw new Error('Could not get customer');  
-        }
-
-        const {url} = await stripe.billingPortal.sessions.create({
-            customer,
-            return_url: `${getURL()}/account`
-        });
-
-        return NextResponse.json({url})
-    } catch (error) {
-        console.log(error);
-        return new NextResponse('Internal error',{status: 500});
-    };
+    return NextResponse.json({ url });
+  } catch (err: any) {
+    console.log(err);
+    new NextResponse('Internal Error', { status: 500 })
+  }
 };
