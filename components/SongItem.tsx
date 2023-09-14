5fd8d0bb3b14c9c8ff7fef323b7f351cf5f51b64
jumma <aarifhingorja4@gmail.com>
2023-09-14 07:31:46 +0530

    fix type error

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -13,7 +13,7 @@ interface SongItemProps {
 }
 
 const SongItem: FC<SongItemProps> = ({ data, onClick }) => {
-    const imagePath = useLoadImage(data);
+    const imagePath = useLoadImage(data) as string;
     return (
         <div onClick={() => onClick(data.id)} className='relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5  cursor-pointer hover:bg-neutral-400/10 transition p-3 '>
             <div className='
